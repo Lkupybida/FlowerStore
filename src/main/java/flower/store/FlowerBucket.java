4f178985fbbc98620fcd79d1d10e39c53fac5545
Lev <lkupybida@gmail.com>
2023-10-12 21:29:01 +0300

    Update FlowerBucket.java

--- src/main/java/flower/store/FlowerBucket.java
+++ src/main/java/flower/store/FlowerBucket.java
@@ -1,18 +1,18 @@
 package flower.store;
 
 import lombok.Getter;
-
+import java.util.ArrayList;
+import java.util.List;
 public class FlowerBucket {
     @Getter
     private int price = 0;
 
     public FlowerBucket() {
     }
+    private final List<FlowerPack> flowerPacks = new ArrayList<>();
 
-    public void price(FlowerPack flowerPack) {
-        price += flowerPack.getPrice();
-    }
-
-    public void add(FlowerPack flowerPack) {
+    public void add(FlowerPack fp) {
+        flowerPacks.add(fp);
+        price += fp.getPrice();
     }
 }
