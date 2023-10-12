4c1c20a8502d9ffa838fcc05ed550b88b29aadbe
Lev <lkupybida@gmail.com>
2023-10-12 22:34:57 +0300

    Fixed flower bucked test so that it runs

--- src/main/java/flower/store/FlowerPack.java
+++ src/main/java/flower/store/FlowerPack.java
@@ -1,13 +1,22 @@
 package flower.store;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class FlowerPack {
-    private Flower flower;
+
+    private final Flower flower;
     private int quantity;
 
-    public FlowerPack(Flower flower, int quantity) {
+    public <T extends Flower> FlowerPack(final T flower,
+            final int quantity) {
+        this.flower = flower;
+        this.quantity = quantity;
     }
 
-    public int getPrice() {
-        return (int) flower.getPrice() * quantity;
+    public double getPrice() {
+        return flower.getPrice() * quantity;
     }
 }
